026116ede6467353d86ee80af38e3e5176a5efa6
Cloud-Lee <cloud1027@gmail.com>
2021-08-27 13:20:27 +0800

    [Modify] 75_pro key mapping update.

--- keyboards/massdrop/4x4/keymaps/via/keymap.c
+++ keyboards/massdrop/4x4/keymaps/via/keymap.c
@@ -49,17 +49,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #ifdef ENCODER_ENABLE
 bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) { /* First encoder */
+    /* First encoder */
+    if (index == 0) {
         if (IS_LAYER_ON(0)) {
             if (clockwise) {
                 //tap_code(KC_VOLU);
                 rgblight_increase_val();
-                //led_matrix_mode(RGB_MATRIX_BREATHING);
             } else {
                 //tap_code(KC_VOLD);
                 rgblight_decrease_val();
             }
-#if 1
+#if 0
             dprintf("mode = %d\n", rgb_matrix_get_mode());
             dprintf("hue = %d\n", rgb_matrix_get_hue());
             dprintf("sat = %d\n", rgb_matrix_get_sat());
@@ -74,7 +74,7 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 
 #if 1
 void matrix_init_user(void) {
-    debug_enable = true; // for dprintf() enable or not
+    //debug_enable = true; // for dprintf() enable or not
     rgblight_enable();
     rgblight_mode(RGB_MATRIX_BREATHING);
     rgblight_sethsv(255,0,255);
